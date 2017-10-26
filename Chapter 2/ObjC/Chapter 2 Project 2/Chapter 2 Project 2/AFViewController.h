7ae1538917d639e3e2b66c0a65ff13e36602069c
Greg Delgado <grd888@gmail.com>
2017-10-26 14:22:12 +0800

    Chapter 2 Project 2 Obj C

---
+++ Chapter 2/ObjC/Chapter 2 Project 2/Chapter 2 Project 2/AFViewController.h
@@ -0,0 +1,13 @@
+//
+//  AFViewController.h
+//  Chapter 2 Project 2
+//
+//  Created by Greg Delgado on 26/10/2017.
+//  Copyright © 2017 Greg Delgado. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AFViewController : UICollectionViewController
+
+@end
---
+++ Chapter 2/ObjC/Chapter 2 Project 2/Chapter 2 Project 2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Chapter 2 Project 2
+//
+//  Created by Greg Delgado on 26/10/2017.
+//  Copyright © 2017 Greg Delgado. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
