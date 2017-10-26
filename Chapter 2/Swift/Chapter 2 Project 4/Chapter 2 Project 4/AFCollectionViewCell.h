80d08b4590746678798ef568f55dcebb15d8ef8e
Greg Delgado <grd888@gmail.com>
2017-10-26 15:29:39 +0800

    Chapter 2 Project 4 ObjC

---
+++ Chapter 2/Swift/Chapter 2 Project 4/Chapter 2 Project 4/AFCollectionViewCell.h
@@ -0,0 +1,15 @@
+//
+//  AFCollectionViewCell.h
+//  Chapter 2 Project 4
+//
+//  Created by Greg Delgado on 26/10/2017.
+//  Copyright © 2017 Greg Delgado. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AFCollectionViewCell : UICollectionViewCell
+
+@property (nonatomic, copy) NSString *text;
+
+@end
---
+++ Chapter 2/Swift/Chapter 2 Project 4/Chapter 2 Project 4/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Chapter 2 Project 4
+//
+//  Created by Greg Delgado on 26/10/2017.
+//  Copyright © 2017 Greg Delgado. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
